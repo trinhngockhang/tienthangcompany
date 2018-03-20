8fda3bcd8cd61718851dff9a90bdda10f9c46f07
trinhngockhang <trinhngockhang1503@gmail.com>
2018-03-20 15:43:16 +0700

    fix

--- components/BreadCrumbs.js
+++ components/BreadCrumbs.js
@@ -8,7 +8,7 @@ export default () => {
 			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft animated" data-wow-delay=".5s" style={{visibility: 'visible', animationDelay: 0.5, animationName: 'slideInLeft'}}>
 				<li><a href="index.html"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
 				<li class="active">Products</li>
-			</ol>
+			</ol> 
 		</div>
 	</div>
     </div >
